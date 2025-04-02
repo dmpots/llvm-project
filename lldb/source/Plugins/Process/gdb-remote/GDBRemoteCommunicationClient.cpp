8629652a06505f4da193acd3a4947f331f33d78b
Greg Clayton <gclayton@fb.com>
2025-04-02 10:33:15 -0700

    Made the mock GPU plug-in functional.
    This patch implements that changes needed to allow a GPU plug-in to
    detect it is loadable and return a connection URL back to LLDB. LLDB
    will attach to the GPU process automatically by having lldb-server
    listen on port 0, figure out the real port and send it back to LLDB.
    ProcessGDBRemote will create a new target and attach to it
    automatically.

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
@@ -1242,8 +1242,8 @@ bool GDBRemoteCommunicationClient::GetHostInfo(bool force) {
   Log *log = GetLog(GDBRLog::Process);
 
   if (force || m_qHostInfo_is_valid == eLazyBoolCalculate) {
-    // host info computation can require DNS traffic and shelling out to external processes.
-    // Increase the timeout to account for that.
+    // host info computation can require DNS traffic and shelling out to
+    // external processes. Increase the timeout to account for that.
     ScopedTimeout timeout(*this, seconds(10));
     m_qHostInfo_is_valid = eLazyBoolNo;
     StringExtractorGDBRemote response;
@@ -2265,7 +2265,7 @@ bool GDBRemoteCommunicationClient::GetCurrentProcessInfo(bool allow_lazy) {
                  !vendor_name.empty()) {
         llvm::Triple triple(llvm::Twine("-") + vendor_name + "-" + os_name);
         if (!environment.empty())
-            triple.setEnvironmentName(environment);
+          triple.setEnvironmentName(environment);
 
         assert(triple.getObjectFormat() != llvm::Triple::UnknownObjectFormat);
         assert(triple.getObjectFormat() != llvm::Triple::Wasm);
@@ -2300,7 +2300,8 @@ bool GDBRemoteCommunicationClient::GetCurrentProcessInfo(bool allow_lazy) {
         }
         m_process_arch.GetTriple().setVendorName(llvm::StringRef(vendor_name));
         m_process_arch.GetTriple().setOSName(llvm::StringRef(os_name));
-        m_process_arch.GetTriple().setEnvironmentName(llvm::StringRef(environment));
+        m_process_arch.GetTriple().setEnvironmentName(
+            llvm::StringRef(environment));
       }
       return true;
     }
@@ -2928,7 +2929,7 @@ GDBRemoteCommunicationClient::GetCurrentProcessAndThreadIDs(
 
           ids.push_back(*pid_tid);
           ch = response.GetChar(); // Skip the command separator
-        } while (ch == ',');       // Make sure we got a comma separator
+        } while (ch == ','); // Make sure we got a comma separator
       }
     }
 
@@ -4383,3 +4384,5 @@ llvm::Expected<int> GDBRemoteCommunicationClient::KillProcess(lldb::pid_t pid) {
                                  "unexpected response to k packet: %s",
                                  response.GetStringRef().str().c_str());
 }
+
+void GDBRemoteCommunicationClient::SetFilePassingFD(int fd) {}
