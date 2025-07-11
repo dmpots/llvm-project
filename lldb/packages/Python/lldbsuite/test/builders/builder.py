db489f3bd11fa689699674ee57e1a259460c7428
David Peixotto <peix@meta.com>
2025-07-11 14:33:19 -0700

    Rename getRocmArgs to getHipccArgs

--- lldb/packages/Python/lldbsuite/test/builders/builder.py
+++ lldb/packages/Python/lldbsuite/test/builders/builder.py
@@ -241,7 +241,7 @@ class Builder:
             return libcpp_args
         return []
 
-    def getRocmArgs(self):
+    def getHipccArgs(self):
         if configuration.hipcc_path:
             return ["HIPCC={}".format(configuration.hipcc_path)]
         return []
@@ -298,7 +298,7 @@ class Builder:
             self.getSDKRootSpec(),
             self.getModuleCacheSpec(),
             self.getLibCxxArgs(),
-            self.getRocmArgs(),
+            self.getHipccArgs(),
             self.getLLDBObjRoot(),
             self.getCmdLine(dictionary),
         ]
