758ae12bf517f81e019d9027766bc8362d34746c
David Peixotto <peix@meta.com>
2025-07-11 17:03:10 -0700

    Add tests for amdgpu lldb-server plugin (#7)
    * Add tests for amdgpu lldb-server plugin
    
    This commit adds some basic unit tests for the AMDGPU plugin.
    It modifies the test configuration to expose the HIPCC compiler
    to the make file when the AMDGPU plugin is enabled. It also modifies
    the dotest framework so that we can compile .hip files using the
    Makefile.rules like we do for other tests.
    
    The unit tests compile a simple hip program and validate that we do
    create the gpu target succesfully and that we can hit a breakpoint
    in the gpu code.
    
    * Use dynamic line number for breakpoint
    
    * Use LLDB_ENABLE_AMDGPU_PLUGIN configuration variable
    
    * Rename getRocmArgs to getHipccArgs

--- lldb/packages/Python/lldbsuite/test/dotest_args.py
+++ lldb/packages/Python/lldbsuite/test/dotest_args.py
@@ -1,7 +1,7 @@
 # System modules
 import argparse
-import sys
 import os
+import sys
 import textwrap
 
 # LLDB modules
@@ -65,6 +65,13 @@ def create_parser():
         help=textwrap.dedent(
             """Specify the target triple. Used for cross compilation."""
         ),
+    )
+    group.add_argument(
+        "--hipcc-path",
+        metavar="path",
+        dest="hipcc_path",
+        default="",
+        help=textwrap.dedent("""Specify the path to a ROCM hipcc compiler."""),
     )
     if sys.platform == "darwin":
         group.add_argument(
