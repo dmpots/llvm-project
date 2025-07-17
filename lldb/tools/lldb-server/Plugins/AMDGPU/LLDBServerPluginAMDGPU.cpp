4dccfcab2887a3f0a7919ee8ef80ac83aa26ce81
Walter Erquinigo <werquinigo@nvidia.com>
2025-07-17 17:24:36 -0400

    Update LLDBServerPluginAMDGPU.cpp

--- lldb/tools/lldb-server/Plugins/AMDGPU/LLDBServerPluginAMDGPU.cpp
+++ lldb/tools/lldb-server/Plugins/AMDGPU/LLDBServerPluginAMDGPU.cpp
@@ -386,7 +386,7 @@ std::optional<GPUActions> LLDBServerPluginAMDGPU::NativeProcessIsStopping() {
                        "launched successfully");
       }
       actions.connect_info = CreateConnection();
-      actions.wait_synchronously_for_gpu_to_initialize = true;
+      actions.connect_info.synchronous = true; 
     }
     return actions;
   } else {
