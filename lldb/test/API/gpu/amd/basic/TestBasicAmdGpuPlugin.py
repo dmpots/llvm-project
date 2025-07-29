91106de3178d18dfedddfc89e0eae3f3304ce061
David Peixotto <peix@meta.com>
2025-07-29 13:30:07 -0700

    [NFC][amd] Create a test base class to use for AmdGpu tests (#22)
    The base class has some useful helper functions that can be used to
    simplify writing tests. This PR is simply a refactoring of the existing
    test cases but future PRs will also make use of this functionality.

--- lldb/test/API/gpu/amd/basic/TestBasicAmdGpuPlugin.py
+++ lldb/test/API/gpu/amd/basic/TestBasicAmdGpuPlugin.py
@@ -6,11 +6,10 @@ Basic tests for the AMDGPU plugin.
 import lldb
 import lldbsuite.test.lldbutil as lldbutil
 from lldbsuite.test.lldbtest import *
+from amdgpu_testcase import *
 
 
-class BasicAmdGpuTestCase(TestBase):
-    NO_DEBUG_INFO_TESTCASE = True
-
+class BasicAmdGpuTestCase(AmdGpuTestCaseBase):
     def test_gpu_target_created_on_demand(self):
         """Test that we create the gpu target automatically."""
         self.build()
@@ -23,56 +22,23 @@ class BasicAmdGpuTestCase(TestBase):
         (cpu_target, cpu_process, cpu_thread, cpu_bkpt) = lldbutil.run_to_source_breakpoint(
             self, "// CPU BREAKPOINT - BEFORE LAUNCH", source_spec
         )
-        self.assertEqual(self.dbg.GetTargetAtIndex(0), cpu_target)
+        self.assertEqual(self.get_cpu_target(), cpu_target)
 
-        # Get the GPU target.
-        # This target is created at the first CPU stop.
+        # Make sure the GPU target was created and has the default thread.
         self.assertEqual(self.dbg.GetNumTargets(), 2, "There are two targets")
-        gpu_target = self.dbg.GetTargetAtIndex(1)
-        gpu_thread = gpu_target.GetProcess().GetThreadAtIndex(0)
+        gpu_thread = self.get_gpu_process().GetThreadAtIndex(0)
         self.assertEqual(gpu_thread.GetName(), "AMD Native Shadow Thread", "GPU thread has the right name")
 
     def test_gpu_breakpoint_hit(self):
-        """Test that we create the gpu target automatically."""
+        """Test that we can hit a breakpoint on the gpu target."""
         self.build()
 
-        # Set a breakpoint in the CPU source and run to it.
+        # GPU breakpoint should get hit by at least one thread.
         source = "hello_world.hip"
-        source_spec = lldb.SBFileSpec(source, False)
-        (cpu_target, cpu_process, cpu_thread, cpu_bkpt) = lldbutil.run_to_source_breakpoint(
-            self, "// CPU BREAKPOINT - BEFORE LAUNCH", source_spec
+        gpu_threads = self.run_to_gpu_breakpoint(
+            source, "// GPU BREAKPOINT", "// CPU BREAKPOINT - BEFORE LAUNCH"
         )
-
-        # Switch to the GPU target so we can set a breakpoint.
-        gpu_target = self.dbg.GetTargetAtIndex(1)
-        gpu_process = gpu_target.GetProcess()
-        self.dbg.SetSelectedTarget(gpu_target)
-
-
-        # Set a breakpoint in the GPU source.
-        # This will not yet resolve to a location.
-        line = line_number(source, "// GPU BREAKPOINT")
-        gpu_bkpt = lldbutil.run_break_set_by_file_and_line(
-            self, source, line, num_expected_locations=0, loc_exact=False
-        )
-
-        # Need to run these commands asynchronously to be able to switch targets.
-        self.setAsync(True)
-        listener = self.dbg.GetListener()
-
-        # Continue the GPU process.
-        self.runCmd("c")
-        lldbutil.expect_state_changes(self, listener, gpu_process, [lldb.eStateRunning])
-
-        # Continue the CPU process.
-        self.dbg.SetSelectedTarget(cpu_target)
-        self.runCmd("c")
-        lldbutil.expect_state_changes(self, listener, cpu_process, [lldb.eStateRunning])
-
-        # GPU breakpoint should get hit.
-        lldbutil.expect_state_changes(self, listener, gpu_process, [lldb.eStateStopped])
-        threads = lldbutil.get_threads_stopped_at_breakpoint_id(gpu_process, gpu_bkpt)
-        self.assertNotEqual(None, threads, "GPU thread should be stopped at breakpoint")
+        self.assertNotEqual(None, gpu_threads, "GPU should be stopped at breakpoint")
 
     def test_no_unexpected_stop(self):
         """Test that we do not unexpectedly hit a stop in the debugger when
