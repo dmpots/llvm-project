ae9e71e7e5ab3baecbc6c3ac49529dfea55e3e47
David Peixotto <peix@meta.com>
2025-09-26 09:39:16 -0700

    [AMD][GPU] Return processed events from process_event_queue (#49)
    This commit modifies the process_event_queue function to return the set of events that were processed. It also pushes the acknowledgment of the event into the handleGPUEvent function so that all events will be acknowledge once they are handled.
    
    We also modify the process_event_queue function to give the caller the choice of how they want to handle the until_event target event. We can either not handle the event (which was the previous semantics) or choose to handle the event (which is the new default). We were not taking advanatge of delaying the handling of the event so I changed the default to handle the target event.
    
    This is used in an upcoming change to make it easier to implement lanes-as-threads by having an explicit check to know when we have hit a wave breakpoint instead of checking for the modification of an internal data structure.

---
+++ lldb/tools/lldb-server/Plugins/AMDGPU/AmdDbgApiHelpers.h
@@ -0,0 +1,92 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains helpers for working with the amd-dbgapi.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef LLDB_TOOLS_LLDB_SERVER_AMDDBGAPIHELPERS_H
+#define LLDB_TOOLS_LLDB_SERVER_AMDDBGAPIHELPERS_H
+#include <algorithm>
+#include <amd-dbgapi/amd-dbgapi.h>
+#include <string>
+#include <vector>
+
+namespace lldb_private {
+namespace lldb_server {
+static const char *AmdDbgApiEventKindToString(amd_dbgapi_event_kind_t kind) {
+  switch (kind) {
+  case AMD_DBGAPI_EVENT_KIND_NONE:
+    return "NONE";
+
+  case AMD_DBGAPI_EVENT_KIND_WAVE_STOP:
+    return "WAVE_STOP";
+
+  case AMD_DBGAPI_EVENT_KIND_WAVE_COMMAND_TERMINATED:
+    return "WAVE_COMMAND_TERMINATED";
+
+  case AMD_DBGAPI_EVENT_KIND_CODE_OBJECT_LIST_UPDATED:
+    return "CODE_OBJECT_LIST_UPDATED";
+
+  case AMD_DBGAPI_EVENT_KIND_BREAKPOINT_RESUME:
+    return "BREAKPOINT_RESUME";
+
+  case AMD_DBGAPI_EVENT_KIND_RUNTIME:
+    return "RUNTIME";
+
+  case AMD_DBGAPI_EVENT_KIND_QUEUE_ERROR:
+    return "QUEUE_ERROR";
+  }
+  assert(false && "unhandled amd_dbgapi_event_kind_t value");
+}
+
+struct AmdDbgApiEventSet {
+  AmdDbgApiEventSet() = default;
+
+  void SetLastEvent(amd_dbgapi_event_id_t event_id,
+                    amd_dbgapi_event_kind_t event_kind) {
+    m_last_event_id = event_id;
+    m_events.push_back(event_kind);
+  }
+
+  amd_dbgapi_event_id_t GetLastEventID() const { return m_last_event_id; }
+
+  bool HasEvent(amd_dbgapi_event_kind_t event_kind) const {
+    return std::find(m_events.begin(), m_events.end(), event_kind) !=
+           m_events.end();
+  }
+
+  bool HasWaveStopEvent() const {
+    return HasEvent(AMD_DBGAPI_EVENT_KIND_WAVE_STOP);
+  }
+
+  bool HasBreakpointResumeEvent() const {
+    return HasEvent(AMD_DBGAPI_EVENT_KIND_BREAKPOINT_RESUME);
+  }
+
+  std::string ToString() const {
+    std::string s("[");
+    for (size_t i = 0; i < m_events.size(); ++i) {
+      if (i != 0)
+        s += ", ";
+      s += AmdDbgApiEventKindToString(m_events[i]);
+    }
+    return s + "]";
+  }
+
+private:
+  // Keep track of events we have seen.
+  // We use a vector to aid in debugging so that we can track both the
+  // order of events and allow duplicates.
+  std::vector<amd_dbgapi_event_kind_t> m_events;
+  amd_dbgapi_event_id_t m_last_event_id = AMD_DBGAPI_EVENT_NONE;
+};
+} // namespace lldb_server
+} // namespace lldb_private
+#endif
