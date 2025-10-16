d6c1f604f61f9ecc7bbc0b8354830985710d0bf1
David Peixotto <peix@meta.com>
2025-10-16 10:51:39 -0700

    Switch pid_t to tid_t

--- lldb/tools/lldb-server/Plugins/AMDGPU/WaveAMDGPU.cpp
+++ lldb/tools/lldb-server/Plugins/AMDGPU/WaveAMDGPU.cpp
@@ -24,15 +24,15 @@ using namespace lldb_server;
 // Reserve a contiguous range of thread IDs for a wave.
 // This is used to have a unique thread ID for each thread.
 // Returns the base thread ID for the wave.
-static lldb::pid_t ReserveTidsForWave(size_t num_lanes) {
+static lldb::tid_t ReserveTidsForWave(size_t num_lanes) {
   // Static counter for thread ids.
   // Skip over the id that is reserved for the shadow thread.
   // This is not a thread-safe variable, but that should be ok since we
   // only have a lldb-server single thread in the main loop.
-  static lldb::pid_t g_tid = ThreadAMDGPU::AMDGPU_SHADOW_THREAD_ID + 1;
+  static lldb::tid_t s_tid = ThreadAMDGPU::AMDGPU_SHADOW_THREAD_ID + 1;
 
-  lldb::pid_t tid_base = g_tid;
-  g_tid += num_lanes;
+  lldb::tid_t tid_base = s_tid;
+  s_tid += num_lanes;
   return tid_base;
 }
 
@@ -41,11 +41,11 @@ void WaveAMDGPU::AddThreadsToList(
     std::vector<std::unique_ptr<NativeThreadProtocol>> &threads) {
 
   size_t num_lanes = llvm::popcount(m_wave_info.exec_mask);
-  lldb::pid_t tid_base = ReserveTidsForWave(num_lanes);
+  lldb::tid_t tid_base = ReserveTidsForWave(num_lanes);
 
   // Create a thread for each lane in the wave.
   for (size_t i = 0; i < num_lanes; ++i) {
-    lldb::pid_t tid = tid_base + i;
+    lldb::tid_t tid = tid_base + i;
     threads.push_back(
         std::make_unique<ThreadAMDGPU>(process, tid, shared_from_this()));
   }
