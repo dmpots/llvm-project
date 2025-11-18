f7d4445aaa9f256d7a7c6723259c79c559e54849
David Peixotto <peix@meta.com>
2025-11-18 12:33:33 -0800

    [GPU][AMD] Add support for address space reads
    This commit enables the amd gpu process to read from different address
    spaces. We the following address spaces:
    
      * global - default address space
      * generic - flat address space
      * region - gds address space (not supported on MI300/MI350)
      * local - local (shared memory) address space
      * private_lane - thread local address space
      * private_wave - wave local address space
    
    These address spaces correspond to address spaces exposed for dwarf
    expressions.
    
    In order to read from the private_lane address space we need to keep the
    lane as part of the ThreadAMDGPU object. This is a straightforward
    modification to the existing code that creates the per-lane threads.
    
    Tests were added to exercise each of the exposed address spaces.

--- lldb/packages/Python/lldbsuite/test/tools/gpu/amdgpu_testcase.py
+++ lldb/packages/Python/lldbsuite/test/tools/gpu/amdgpu_testcase.py
@@ -2,17 +2,19 @@ from lldbsuite.test.tools.gpu.gpu_testcase import GpuTestCaseBase
 import lldb
 from lldbsuite.test import lldbutil
 from lldbsuite.test.lldbtest import line_number
+from typing import List
 
 
 class AmdGpuTestCaseBase(GpuTestCaseBase):
     """
     Class that should be used by all python AMDGPU tests.
     """
+
     NO_DEBUG_INFO_TESTCASE = True
 
     def run_to_gpu_breakpoint(
-        self, source, gpu_bkpt_pattern, cpu_bkpt_pattern
-    ):
+        self, source: str, gpu_bkpt_pattern: str, cpu_bkpt_pattern: str
+    ) -> List[lldb.SBThread]:
         """Run the test executable unit it hits the provided GPU breakpoint.
         The CPU breakpoint is used as a stopping point to switch to the GPU target
         and set the GPU breakpoint on that target.
@@ -24,6 +26,12 @@ class AmdGpuTestCaseBase(GpuTestCaseBase):
         )
         self.assertEqual(cpu_target, self.cpu_target)
 
+        gpu_bkpt_id = self.set_gpu_source_breakpoint(source, gpu_bkpt_pattern)
+
+        return self.continue_to_gpu_breakpoint(gpu_bkpt_id)
+
+    def set_gpu_source_breakpoint(self, source: str, gpu_bkpt_pattern: str) -> int:
+        """Set a breakpoint on the gpu target. Returns the breakpoint id."""
         # Switch to the GPU target so we can set a breakpoint.
         self.assertTrue(self.gpu_target.IsValid())
         self.select_gpu()
@@ -32,23 +40,47 @@ class AmdGpuTestCaseBase(GpuTestCaseBase):
         # This might not yet resolve to a location so use -2 to not check
         # for the number of locations.
         line = line_number(source, gpu_bkpt_pattern)
-        gpu_bkpt = lldbutil.run_break_set_by_file_and_line(
+        return lldbutil.run_break_set_by_file_and_line(
             self, source, line, num_expected_locations=-2, loc_exact=False
         )
 
+    def continue_to_gpu_breakpoint(self, gpu_bkpt_id: int) -> List[lldb.SBThread]:
+        """Continues execution on the cpu and gpu until we hit the gpu breakpoint"""
         # Need to run these commands asynchronously to be able to switch targets.
         self.setAsync(True)
         listener = self.dbg.GetListener()
 
         # Continue the GPU process.
         self.runCmd("c")
-        lldbutil.expect_state_changes(self, listener, self.gpu_process, [lldb.eStateRunning])
+        lldbutil.expect_state_changes(
+            self, listener, self.gpu_process, [lldb.eStateRunning]
+        )
 
         # Continue the CPU process.
         self.select_cpu()
         self.runCmd("c")
-        lldbutil.expect_state_changes(self, listener, self.cpu_process, [lldb.eStateRunning])
+        lldbutil.expect_state_changes(
+            self, listener, self.cpu_process, [lldb.eStateRunning]
+        )
 
         # GPU breakpoint should get hit.
-        lldbutil.expect_state_changes(self, listener, self.gpu_process, [lldb.eStateStopped])
-        return lldbutil.get_threads_stopped_at_breakpoint_id(self.gpu_process, gpu_bkpt)
+        lldbutil.expect_state_changes(
+            self, listener, self.gpu_process, [lldb.eStateStopped]
+        )
+        return lldbutil.get_threads_stopped_at_breakpoint_id(
+            self.gpu_process, gpu_bkpt_id
+        )
+
+    def continue_to_gpu_source_breakpoint(
+        self, source: str, gpu_bkpt_pattern: str
+    ) -> List[lldb.SBThread]:
+        """
+        Sets a gpu breakpoint set by source regex gpu_bkpt_pattern, continues the process, and deletes the breakpoint again.
+        Otherwise the same as `continue_to_gpu_breakpoint`.
+        Inspired by lldbutil.continue_to_source_breakpoint.
+        """
+        gpu_bkpt_id = self.set_gpu_source_breakpoint(source, gpu_bkpt_pattern)
+        gpu_threads = self.continue_to_gpu_breakpoint(gpu_bkpt_id)
+        self.gpu_target.BreakpointDelete(gpu_bkpt_id)
+
+        return gpu_threads
